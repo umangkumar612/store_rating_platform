1cab18b0bca7461fc16ddcfe900eac8fd0543958
umang___kataria__ <umangkumarravi123@gmail.com>
2026-05-08 00:27:05 +0530

    Fixed CORS for Vercel frontend

--- server/src/middleware/security.js
+++ server/src/middleware/security.js
@@ -6,7 +6,11 @@ import { env } from "../config/env.js";
 export const helmetMiddleware = helmet();
 
 export const corsMiddleware = cors({
-  origin: env.clientUrl,
+  origin: [
+    "http://localhost:5173",
+    "https://storeratingplatform.vercel.app",
+    "https://storeratingplatform-17xt3ub10-umang-kumars-projects-4cbc98e6.vercel.app"
+  ],
   credentials: true
 });
 
